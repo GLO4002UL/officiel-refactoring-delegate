90ca757f01d0f26ccc95001c33989e4be5787d66
Jni <jni.viens@gmail.com>
2023-09-17 21:49:06 -0400

    Initial commit

---
+++ src/main/java/ca/ulaval/glo4002/delegate/Animal.java
@@ -0,0 +1,13 @@
+package ca.ulaval.glo4002.delegate;
+
+public class Animal {
+
+  public void move() {
+    System.out.println("je marche à quatre pattes.");
+  }
+
+  public void eat() {
+    System.out.println("je mange de la viande.");
+  }
+}
+
---
+++ src/main/java/ca/ulaval/glo4002/delegate/Lion.java
@@ -0,0 +1,5 @@
+package ca.ulaval.glo4002.delegate;
+
+public class Lion
+    extends Animal {
+}
